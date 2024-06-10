320747a8d48fb01b70341f74a06fc305f4298b7b
oksana <0kssana.sdm@gmail.com>
2024-06-10 23:11:33 +0300

    база куб

---
+++ RCube/glcube.cpp
@@ -0,0 +1,16 @@
+//
+// Created by oksana on mew/mew/24.
+//
+#include "glcube.h"
+#include <iostream>
+#include <cstring>
+
+//--CUBIE--
+std::vector<std::vector<int>> d_state = {
+        {0,0,0,0,0,0,0,0,0},
+        {4,4,4,4,4,4,4,4,4},
+        {1,1,1,1,1,1,1,1,1},
+        {5,5,5,5,5,5,5,5,5},
+        {3,3,3,3,3,3,3,3,3},
+        {2,2,2,2,2,2,2,2,2},
+};
