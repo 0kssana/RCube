4bbabe47dd646242c73f21a14287f85cf0f00572
oksana <0kssana.sdm@gmail.com>
2024-06-12 22:57:32 +0300

    method main and another helpers fn

---
+++ RCube/main.cpp
@@ -0,0 +1,29 @@
+#include "cube.h"
+Cube my_cube;
+
+void specialKeys(int key, int x, int y){
+    my_cube.specialKeys_f(key, x, y);
+}
+
+void normalKeys(unsigned char key, int x, int y){
+    my_cube.normalKeys_f(key, x, y);
+}
+
+void display(){
+    my_cube.display_f();
+}
+
+int main(int argc, char* argv[]){
+    glutInit(&argc,argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowSize(500,500);
+    glutCreateWindow("Cube");
+    glEnable(GL_DEPTH_TEST);
+    glClearColor(0.2,0.2,0.2,1);
+    glutDisplayFunc(display);
+    glutKeyboardFunc(normalKeys);
+    glutSpecialFunc(specialKeys);
+    glutMainLoop();
+    return 0;
+
+}
